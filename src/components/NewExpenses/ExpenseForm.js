4072d9752656b3aa09a5181a5195334d1c7ce43c
Mohim-Singla <mohimsingla@gmail.com>
2021-06-04 01:08:56 +0530

    Handling Form Submission

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -28,22 +28,25 @@ const ExpenseForm = () => {
             Date: new Date(NewDate)
         };
         console.log(expenseData);
+        setNewTitle('');
+        setNewAmount('');
+        setNewDate('');
     };
     return (
 
         <form onSubmit={SubmitHandler}>
             <div className="new-expense__controls">
-                <div className="new-expense__control" onChange={setNTitle}>
+                <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" />
+                    <input value={NewTitle} type="text" onChange={setNTitle} />
                 </div>
-                <div className="new-expense__control" onChange={setNAmount}>
+                <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" value={NewAmount} min="0.01" step="0.01" onChange={setNAmount} />
                 </div>
-                <div className="new-expense__control" onChange={setNDate}>
+                <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                    <input type="date" value={NewDate} min="2019-01-01" max="2022-12-31" onChange={setNDate} />
                 </div>
                 <div className="new-expense__actions">
                     <button type='submit'>Add Expense</button>
